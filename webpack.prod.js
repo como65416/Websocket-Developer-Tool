33d60cdcebdbec1ac182345e86e22db094c16d2f
comoco <como65416@gmail.com>
2019-05-19 15:57:21 +0800

    replace uglifyjs-webpack-plugin to terser-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { VueLoaderPlugin } = require('vue-loader');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
@@ -43,8 +43,8 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
+      new TerserPlugin({
+        terserOptions: {
           warnings: false,
           parse: {},
           compress: {},
