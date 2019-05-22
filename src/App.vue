3d1dcf7520c644d328b90729bdc040eac3aef8f0
comoco <como65416@gmail.com>
2019-05-22 22:43:09 +0800

    add App.vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <AddDialog @onConnectCreated="onConnectCreated"></AddDialog>
+    <el-tabs tab-position="left" type="border-card">
+      <el-tab-pane v-for="tabData in tabDatas" :key="tabData.id">
+        <span slot="label">{{ tabData.name }}</span>
+        <SocketConsole :webSocket="tabData.webSocket"></SocketConsole>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import AddDialog from "./components/AddDialog.vue";
+import SocketConsole from "./components/SocketConsole.vue";
+
+export default {
+  data() {
+    return {
+      nextTabId: 1,
+      addConnectDialog: true,
+      tabDatas: []
+    };
+  },
+  methods: {
+    onConnectCreated: function (name, webSocket) {
+      this.tabDatas.push({
+        id: this.nextTabId++,
+        name: name,
+        webSocket: webSocket
+      });
+    }
+  },
+  components: {
+    AddDialog: AddDialog,
+    SocketConsole: SocketConsole
+  }
+}
+</script>
